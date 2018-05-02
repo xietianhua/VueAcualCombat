867b97460e0fe20cfa17adda10a3a6f43b854c4c
home <tianhua_cn@163.com>
2018-05-02 23:07:45 +0800

    更新

--- src/components/Pos.vue
+++ src/components/Pos.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pos">
-    <el-col :span="7">
+    <el-col :span="7" class="pos-order" id="order-list">
        我是订单栏
     </el-col>
     <!--商品展示-->
@@ -15,13 +15,22 @@
   import ElCol from "element-ui/packages/col/src/col";
 
   export default {
-    components: {ElCol}
-
+    name:'pos',
+    components: {ElCol},
+    mounted:function () {
+      var orderHeight=document.body.clientHeight;
+      console.log(orderHeight);
+      document.getElementById('order-list').style.height=orderHeight+"px";
+    }
   };
 </script>
 
 
 <style>
+  .pos-order{
+    background-color: #f9fafc;
+    border-right:1px solid #ddd;
+  }
 
 </style>
 
